0732e9bda1c6055dfda3413a481799234c785fda
Sakhawat hosen <sakhawat55cmt@outlook.com>
2019-10-12 22:53:54 +0600

    AppService fix

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class HeroService {
   [x: string]: any;
 
-  constructor(
-    private http: HttpClient,
-    private messageService: MessageService) 
-    {
+  constructor( private http: HttpClient, private messageService: MessageService) 
+  {
 
-    }
+  }
 
     
 
@@ -28,11 +27,12 @@ export class HeroService {
   //   return of(HEROES);
   // }
 
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
-  }
+  /** GET heroes from the server */
+  // getHeroes(): Observable<Hero[]> {
+  //   // TODO: send the message _after_ fetching the heroes
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return of(HEROES);
+  // }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
@@ -40,8 +40,34 @@ export class HeroService {
     return of(HEROES.find(hero => hero.id === id));
   }
 
+  // HTTP Service Start
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  // getHeroes(): Observable<Hero[]> {
+  //   return of(HEROES);
+  // }
+
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    //this.messageService.add('HeroService: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+  //Http Service end
 
 }
